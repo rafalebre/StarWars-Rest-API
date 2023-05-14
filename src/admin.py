a924364c46b12e8ec34973ee80be004e5fbae4ee
rafalebre <rafaellebre100@gmail.com>
2023-05-14 10:40:32 +0000

    Planet, Person, Favorite added in admin.py

--- src/admin.py
+++ src/admin.py
@@ -1,12 +1,15 @@
 import os
 from flask_admin import Admin
-from models import db, User
+from models import db, User, Planet, Person, Favorite
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Person, db.session))
+    admin.add_view(ModelView(Favorite, db.session))
 
     
     # Add your models here, for example this is how we add a the User model to the admin
