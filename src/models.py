51cd11dbd0f6bdda1c854a0f02cc631d2e4c2a90
rafalebre <rafaellebre100@gmail.com>
2023-05-14 10:26:00 +0000

    added models for planets, people, favorites

--- src/models.py
+++ src/models.py
@@ -17,3 +17,48 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+    def serialize(self):
+            return {
+                "id": self.id,
+                "name": self.name,
+            }
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Person %r>' % self.name
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('favorites', lazy=True))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    planet = db.relationship('Planet', backref=db.backref('favorites', lazy=True))
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
+    person = db.relationship('Person', backref=db.backref('favorites', lazy=True))
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "person_id": self.person_id,
+        }
